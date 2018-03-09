2d76790432f09c14a3bbc10b2dd08f45f535d830
Samuel Laroche <samuel.laroche@hrsdc-rhdcc.gc.ca>
2018-03-09 15:00:03 -0500

    update app

--- server.js
+++ server.js
@@ -5,32 +5,63 @@ var querystring = require('querystring');
 var fs = require('fs');
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const $PORT = process.env.port || 8080;
 
 app.get('/', function(req,res){
-    
-    fs.readFile('menu.html', 'utf8', function(err,content){
+
+    fs.readFile('index.html', 'utf8', function(err,content){
         res.send(content);
     })
 })
 
+//Here are handled solution request
 app.get('/solution', function(req, res){
     var id = req.query.id;
-    
-    fs.readFile('res/solutionYearly' + id + '.html', 'utf8', function(err,content){
-        
-    if (err){
-        res.send('Not found');
-    } else  {
-        res.send(content);
+
+    if (id) { //Detect if id is filled, return chart otherwise send menu
+
+      fs.readFile('res/solutionYearly' + id + '.html', 'utf8', function(err,content){
+        if (err){
+          res.send('Not found');
+        } else  {
+          res.send(content);
+        }
+      })
+
+    } else {
+
+      fs.readFile("solution.html", 'utf8', function(err,content){
+          res.send(content);
+      })
     }
-    })
+})
+
+//Here are handled projects requests
+app.get('/project', function(req, res){
+
+    var fy = req.query.fiscalyear
+    var id = req.query.id;
+
+    if (fy || id) { //Return chart if both fields are filled, otherwise return menu
+
+      fs.readFile('res/Project' + id + '-' + fy + '.html', 'utf8', function(err,content){
+        if (err){
+          res.send('Not found');
+        } else  {
+          res.send(content);
+        }
+      })
+
+    } else {
+        fs.readFile("projet.html", 'utf8', function(err,content){
+          res.send(content);
+        })
+      }
 })
 
 app.listen($PORT, function(){
     console.log('Server listening on port ' + $PORT);
 })
-
