b30898b231d7dad0f5c51a7c4e39bc6dcd05c184
Samuel Laroche <samuel.laroche@hrsdc-rhdcc.gc.ca>
2018-04-09 15:32:33 -0400

    restored projectPeriod.sql

---
+++ sql/projectPeriod.sql
@@ -0,0 +1,9 @@
+SELECT p.Name As Context, Activity.Name, Direct.Period, SUM(Direct.Amount) AS Amount
+FROM Direct
+        INNER JOIN Network n ON Direct.Object = n.ID
+        INNER JOIN WBS w on w.ID = n.WBS
+        INNER JOIN ProjectDefinition p ON w.ProjectDefinition = p.ID
+        INNER JOIN Activity ON Activity.ID = Direct.NetworkActivity
+WHERE p.ID=? AND Direct.FiscalYear=?
+GROUP BY Activity.Name, Direct.Period
+ORDER BY FIELD (Activity.Name, 'Project Management','Analyse','Plan','Design','Build','Test','Deploy','Stabilize/Close');
